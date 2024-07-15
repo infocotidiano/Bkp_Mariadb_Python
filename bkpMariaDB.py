cba5574272016104a696f05eeeef1f2cf3d622ec
Daniel de Morais <danegd@gmail.com>
2024-07-14 23:51:08 -0300

    testes iniciais

---
+++ bkpMariaDB.py
@@ -0,0 +1,26 @@
+# program       bkpMariDB.py
+# author        Daniel de Morais
+# installarion  Backup base de dados Banco MariaDB
+# date-written  14/07/2024
+
+import time
+import os
+import sys
+import subprocess as commands
+
+LDATA    = time.strftime('%Y%m%d_%H%M%S')
+LUSUARIO = 'root'
+LSENHA   = 'Info@1234'
+LPORTA   = 3306
+LBANCO   = 'hermes'
+LDESTINO = 'c:/temp'
+LHOST    = 'localhost'
+LPATHDUMP= 'C:/Program Files/MariaDB 10.9/bin'
+
+LNomeArquivoDestino = "%s/%s-%s.sql" % (LDESTINO, LBANCO, LDATA)
+print('Arquivo de destino: ',LNomeArquivoDestino)
+ComandoBkp =  ("%s/mysqldump -u %s -p%s -h %s -e --opt -B -R -c %s > %s" % (LPATHDUMP,LUSUARIO, LSENHA, LHOST, LBANCO, LNomeArquivoDestino))
+print(ComandoBkp)
+cmd_result = commands.getoutput(ComandoBkp)
+
+
