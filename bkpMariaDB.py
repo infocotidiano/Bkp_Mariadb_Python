e91724b7ba4c0eadffcf121db53ee2af98f479a0
Daniel de Morais <danegd@gmail.com>
2024-07-16 06:52:13 -0300

    criado a rotina para manter somente 5 backup antes realizar um novo backup

--- bkpMariaDB.py
+++ bkpMariaDB.py
@@ -1,5 +1,5 @@
 # program       bkpMariDB.py
-# author        Daniel de Morais
+# author        Daniel de Morais InfoCotidiano
 # installation  Backup base de dados Banco MariaDB
 # date-written  14/07/2024
 
@@ -7,6 +7,7 @@ import time
 import os
 import sys
 import subprocess 
+import glob
 
 LDATA    = time.strftime('%Y%m%d_%H%M%S')
 LUSUARIO = 'root'
@@ -16,7 +17,17 @@ LBANCO   = 'hermes'
 LDESTINO = 'c:/temp'
 LHOST    = 'localhost'
 LPATHDUMP= '"C:/Program Files/MariaDB 10.9/bin/mysqldump"'
+LQTDE_BKP= 5
 
+# Manter 5 Backups mais recentes
+os.chdir(LDESTINO)
+ListaBackups = glob.glob("hermes-*.sql")
+ListaBackups.sort(key=os.path.getmtime, reverse=True)
+for backup in ListaBackups[LQTDE_BKP:]:
+    os.remove(backup)
+    print(f"Apagado: {backup}")
+    
+#Realiza um novo Backup
 LNomeArquivoDestino = "%s/%s-%s.sql" % (LDESTINO, LBANCO, LDATA)
 print('Arquivo de destino: ',LNomeArquivoDestino)
 ComandoBkp =  ("%s -u %s -p%s -h %s -e --opt -B -R -c %s > %s" % (LPATHDUMP,LUSUARIO, LSENHA, LHOST, LBANCO, LNomeArquivoDestino))
@@ -28,5 +39,6 @@ if LProcesso.returncode != 0:
     print(f"Erro ao realizar o backup: {stderr.decode('utf-8')}")
 else:
     print("Backup realizado com sucesso!")
-
+    print("Valide constatemente o seu backup.")
+    print("Use por conta e risco !")
 
