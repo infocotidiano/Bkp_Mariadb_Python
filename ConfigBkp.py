913552b73fdb5a6b1725c3b24557ec250d74d5f4
Daniel de Morais <danegd@gmail.com>
2024-07-16 07:23:38 -0300

    arquivo para configurar o backup

---
+++ ConfigBkp.py
@@ -0,0 +1,22 @@
+import json
+
+def get_input(prompt):
+    return input(prompt).strip()
+
+# Solicitar os parâmetros ao usuário
+config = {
+    "Usuario": get_input("Digite o usuário: "),
+    "Senha": get_input("Digite a senha: "),
+    "Host": get_input("Digite o host: "),
+    "Porta": get_input("Digite a porta: "),
+    "NomeBanco": get_input("Digite o nome do banco: "),
+    "PathDUMP": get_input("Digite o caminho para o DUMP: "),
+    "PastaDestino": get_input("Digite a pasta de destino: "),
+    "QtdeBackup": int(get_input("Digite a quantidade de backups a manter: "))
+}
+
+# Salvar os parâmetros em um arquivo JSON
+with open("config.json", "w") as config_file:
+    json.dump(config, config_file, indent=4)
+
+print("Configurações salvas em config.json")
